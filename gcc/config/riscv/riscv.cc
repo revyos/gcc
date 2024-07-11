77ddd4318e19fa8a5671bc65c95884906c257ed0
xuli <xuli1@eswincomputing.com>
2024-07-11 04:29:11 +0000

    RISC-V: Disable misaligned vector access in hook riscv_slow_unaligned_access[PR115862]
    The reason is that in the following code, icode = movmisalignv8si has
    already been rejected by TARGET_VECTOR_MISALIGN_SUPPORTED, but it is
    allowed by targetm.slow_unaligned_access,which is contradictory.
    
    (((icode = optab_handler (movmisalign_optab, mode))
    	   != CODE_FOR_nothing)
    	  || targetm.slow_unaligned_access (mode, align))
    
    misaligned vector access should be enabled by -mno-vector-strict-align option.
    
    	PR target/115862
    
    gcc/ChangeLog:
    
    	* config/riscv/riscv.cc (riscv_slow_unaligned_access): Disable vector misalign.
    
    Signed-off-by: Li Xu <xuli1@eswincomputing.com>

--- gcc/config/riscv/riscv.cc
+++ gcc/config/riscv/riscv.cc
@@ -9903,9 +9903,10 @@ riscv_cannot_copy_insn_p (rtx_insn *insn)
 /* Implement TARGET_SLOW_UNALIGNED_ACCESS.  */
 
 static bool
-riscv_slow_unaligned_access (machine_mode, unsigned int)
+riscv_slow_unaligned_access (machine_mode mode, unsigned int)
 {
-  return riscv_slow_unaligned_access_p;
+  return VECTOR_MODE_P (mode) ? TARGET_VECTOR_MISALIGN_SUPPORTED
+			      : riscv_slow_unaligned_access_p;
 }
 
 /* Implement TARGET_CAN_CHANGE_MODE_CLASS.  */
