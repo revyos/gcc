faf40ce649a18dabe325731c5d5c53e214bfa027
Yunze Zhu <yunzezhu@linux.alibaba.com>
2025-08-29 11:00:38 +0800

    RISC-V: XuanTie: Add XTheadVcrypto extension
    gcc/ChangeLog:
    
    	* common/config/riscv/riscv-common.cc: Add XTheadVcrypto
    	* config/riscv/riscv-vector-builtins-bases.cc (class th_vcrypto): New.
    	(BASE): New.
    	* config/riscv/riscv-vector-builtins-bases.h: Likewise.
    	* config/riscv/riscv-vector-builtins-shapes.cc (struct th_crypto_def): New.
    	(SHAPE): New.
    	* config/riscv/riscv-vector-builtins-shapes.h: Likewise.
    	* config/riscv/riscv-vector-builtins.cc: Likewise.
    	* config/riscv/riscv-vector-builtins.h (enum required_ext): Likewise.
    	(reqired_ext_to_isa_name): Likewise.
    	(required_extensions_specified): Likewise.
    	(struct function_group_info): Likewise.
    	* config/riscv/riscv.opt: Likewise.
    	* config/riscv/thead-vector-builtins-functions.def (REQUIRED_EXTENSIONS): Likewise.
    	(th_vmacc54l): New.
    	(th_vmacc54h): New.
    	* config/riscv/thead-vector.md (@th_pred_<xtheadvcrypto_insn><xtheadvcrypto_insn_suffix><mode>): New.
    
    gcc/testsuite/ChangeLog:
    
    	* gcc.target/riscv/rvv/rvv.exp: Add XTheadVcrypto tests.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54h_vs_non_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54h_vs_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54h_vv_non_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54h_vv_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54l_vs_non_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54l_vs_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54l_vv_non_overloaded.c: New test.
    	* gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54l_vv_overloaded.c: New test.
    
    Co-authored-by:Jin Ma <jinma@linux.alibaba.com>

---
+++ gcc/testsuite/gcc.target/riscv/rvv/xtheadvcrypto/xtheadvcrypto_vmacc54l_vv_overloaded.c
@@ -0,0 +1,32 @@
+/* { dg-do compile } */
+/* { dg-options "-march=rv32gcv_xtheadvcrypto0p1 -mabi=ilp32d" { target { rv32 } } } */
+/* { dg-options "-march=rv64gcv_xtheadvcrypto0p1 -mabi=lp64d" { target { rv64 } } } */
+
+#include <riscv_vector.h>
+
+vuint64m1_t test0_vmacc54l_vv(vuint64m1_t vd, vuint64m1_t vs2, vuint64m1_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(vd, vs2, vs1, vl);}
+vuint64m1_t test1_vmacc54l_vv_tu(vuint64m1_t vd, vuint64m1_t vs2, vuint64m1_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tu(vd, vs2, vs1, vl);}
+vuint64m1_t test2_vmacc54l_vv_m(vbool64_t mask, vuint64m1_t vd, vuint64m1_t vs2, vuint64m1_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(mask, vd, vs2, vs1, vl);}
+vuint64m1_t test3_vmacc54l_vv_mu(vbool64_t mask, vuint64m1_t vd, vuint64m1_t vs2, vuint64m1_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_mu(mask, vd, vs2, vs1, vl);}
+vuint64m1_t test4_vmacc54l_vv_tum(vbool64_t mask, vuint64m1_t vd, vuint64m1_t vs2, vuint64m1_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tum(mask, vd, vs2, vs1, vl);}
+vuint64m1_t test5_vmacc54l_vv_tumu(vbool64_t mask, vuint64m1_t vd, vuint64m1_t vs2, vuint64m1_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tumu(mask, vd, vs2, vs1, vl);}
+vuint64m2_t test6_vmacc54l_vv(vuint64m2_t vd, vuint64m2_t vs2, vuint64m2_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(vd, vs2, vs1, vl);}
+vuint64m2_t test7_vmacc54l_vv_tu(vuint64m2_t vd, vuint64m2_t vs2, vuint64m2_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tu(vd, vs2, vs1, vl);}
+vuint64m2_t test8_vmacc54l_vv_m(vbool32_t mask, vuint64m2_t vd, vuint64m2_t vs2, vuint64m2_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(mask, vd, vs2, vs1, vl);}
+vuint64m2_t test9_vmacc54l_vv_mu(vbool32_t mask, vuint64m2_t vd, vuint64m2_t vs2, vuint64m2_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_mu(mask, vd, vs2, vs1, vl);}
+vuint64m2_t test10_vmacc54l_vv_tum(vbool32_t mask, vuint64m2_t vd, vuint64m2_t vs2, vuint64m2_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tum(mask, vd, vs2, vs1, vl);}
+vuint64m2_t test11_vmacc54l_vv_tumu(vbool32_t mask, vuint64m2_t vd, vuint64m2_t vs2, vuint64m2_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tumu(mask, vd, vs2, vs1, vl);}
+vuint64m4_t test12_vmacc54l_vv(vuint64m4_t vd, vuint64m4_t vs2, vuint64m4_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(vd, vs2, vs1, vl);}
+vuint64m4_t test13_vmacc54l_vv_tu(vuint64m4_t vd, vuint64m4_t vs2, vuint64m4_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tu(vd, vs2, vs1, vl);}
+vuint64m4_t test14_vmacc54l_vv_m(vbool16_t mask, vuint64m4_t vd, vuint64m4_t vs2, vuint64m4_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(mask, vd, vs2, vs1, vl);}
+vuint64m4_t test15_vmacc54l_vv_mu(vbool16_t mask, vuint64m4_t vd, vuint64m4_t vs2, vuint64m4_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_mu(mask, vd, vs2, vs1, vl);}
+vuint64m4_t test16_vmacc54l_vv_tum(vbool16_t mask, vuint64m4_t vd, vuint64m4_t vs2, vuint64m4_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tum(mask, vd, vs2, vs1, vl);}
+vuint64m4_t test17_vmacc54l_vv_tumu(vbool16_t mask, vuint64m4_t vd, vuint64m4_t vs2, vuint64m4_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tumu(mask, vd, vs2, vs1, vl);}
+vuint64m8_t test18_vmacc54l_vv(vuint64m8_t vd, vuint64m8_t vs2, vuint64m8_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(vd, vs2, vs1, vl);}
+vuint64m8_t test19_vmacc54l_vv_tu(vuint64m8_t vd, vuint64m8_t vs2, vuint64m8_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tu(vd, vs2, vs1, vl);}
+vuint64m8_t test20_vmacc54l_vv_m(vbool8_t mask, vuint64m8_t vd, vuint64m8_t vs2, vuint64m8_t vs1, size_t vl){return __riscv_th_vmacc54l_vv(mask, vd, vs2, vs1, vl);}
+vuint64m8_t test21_vmacc54l_vv_mu(vbool8_t mask, vuint64m8_t vd, vuint64m8_t vs2, vuint64m8_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_mu(mask, vd, vs2, vs1, vl);}
+vuint64m8_t test22_vmacc54l_vv_tum(vbool8_t mask, vuint64m8_t vd, vuint64m8_t vs2, vuint64m8_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tum(mask, vd, vs2, vs1, vl);}
+vuint64m8_t test23_vmacc54l_vv_tumu(vbool8_t mask, vuint64m8_t vd, vuint64m8_t vs2, vuint64m8_t vs1, size_t vl){return __riscv_th_vmacc54l_vv_tumu(mask, vd, vs2, vs1, vl);}
+
+/* { dg-final { scan-assembler-times "th\\.vmacc54l\\.vv\t" 24 } } */
